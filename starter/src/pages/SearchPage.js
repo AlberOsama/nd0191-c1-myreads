15efb97020508c1158be0ef1f9b4cf6653910cf6
AlberOsama <alberosama0@gmail.com>
2022-11-24 23:00:28 +0200

    add search

--- starter/src/pages/SearchPage.js
+++ starter/src/pages/SearchPage.js
@@ -1,4 +1,16 @@
+import { useEffect, useState } from "react";
+import { search } from "../BooksAPI";
+import Book from "../components/Book";
+
 const SearchPage = ({ onClick }) => {
+  const [query, setQuery] = useState("");
+
+  const [books, setBooks] = useState([]);
+  useEffect(async () => {
+    if (query === "") return;
+    setBooks(await search(query, 20));
+  }, [query]);
+
   return (
     <div className="search-books">
       <div className="search-books-bar">
@@ -6,11 +18,21 @@ const SearchPage = ({ onClick }) => {
           Close
         </a>
         <div className="search-books-input-wrapper">
-          <input type="text" placeholder="Search by title, author, or ISBN" />
+          <input
+            type="text"
+            placeholder="Search by title, author, or ISBN"
+            onChange={(e) => setQuery(e.target.value)}
+          />
         </div>
       </div>
       <div className="search-books-results">
-        <ol className="books-grid"></ol>
+        <ol className="books-grid">
+          {books.map((book) => (
+            <li key={book.title}>
+              <Book book={book} />
+            </li>
+          ))}
+        </ol>
       </div>
     </div>
   );
